856f69141c4a71a10cc24e53076d5d4ac65af9cf
EuphSteel01 <112015702+EuphSteel01@users.noreply.github.com>
2022-08-29 13:07:56 -0500

    Create CampaignCostComparator.java

---
+++ Project 11/CampaignCostComparator.java
@@ -0,0 +1,25 @@
+import java.util.Comparator;
+/** DirectMC is derived from MarketingCampaign.
+ *
+ * @author Jess Banister
+ * @version 3/27/21
+ */
+public class CampaignCostComparator implements Comparator<MarketingCampaign> {
+   /**
+    * Compares two MarketingCampaign.
+    * @param c1 first MarketingCampaign
+    * @param c2 second MarketingCampaign
+    * @return int for order
+   */
+   public int compare(MarketingCampaign c1, MarketingCampaign c2) {
+      if (c1.campaignCost() > c2.campaignCost()) {
+         return 1;
+      }
+      else if (c1.campaignCost() < c2.campaignCost()) {
+         return -1;
+      }
+      else {
+         return 0;
+      }
+   }
+}
